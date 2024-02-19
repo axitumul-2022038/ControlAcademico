b08f8d18d2a764b7551c8a7e06e1e56157f798d6
axitumul-2022038 <axitumul-2022038@kinal.edu.gt>
2024-02-19 11:37:55 -0600

    tercer commit

--- src/course/course.routes.js
+++ src/course/course.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { validateJwt, isTeacher } from '../../middlewares/validate-jwt.js';
-import { test, register, update, deleteP } from './person.controller.js';
+import { test, register, update, deleteC } from './course.controller.js';
 
 const api = express.Router();
 
 api.get('/test', [validateJwt, isTeacher], test)
 api.post('/register',[isTeacher], register)
 api.put('/update/:id', [validateJwt, isTeacher], update)
-api.delete('/delete/:id', [validateJwt, isTeacher], deleteP)
+api.delete('/delete/:id', [validateJwt, isTeacher], deleteC)
 
 export default api
